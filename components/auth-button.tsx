f4f89d5ca38cb85d90f3f12850715bbac73d8573
sarinsanyal <sarinsanyal2005@gmail.com>
2026-08-19 15:54:20 +0530

    navbar and DB changes

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
 export const dynamic = "force-dynamic";
 import { createClient } from "@/lib/supabase/server";
-import { LogoutButton } from "./logout-button";
 
 export async function AuthButton() {
   const supabase = await createClient();
-  var firstName = "";
+  let firstName = ""; // Switched to let for standard block scoping
 
   // You can also use getUser() which will be slower.
   const { data } = await supabase.auth.getClaims();
@@ -19,7 +18,12 @@ export async function AuthButton() {
   return user ? (
     <div className="flex items-center gap-4 text-black dark:text-white">
       Hey, {firstName}!
-      <LogoutButton />
+      <Link
+        href="/dashboard"
+        className="group inline-flex items-center gap-2 rounded-full bg-gradient-to-r from-purple-500 to-cyan-400 px-7 py-2.5 text-sm font-semibold transition-transform hover:scale-105 duration-300 text-black"
+      >
+        Dashboard
+      </Link>
     </div>
   ) : (
     <div className="flex gap-2">
